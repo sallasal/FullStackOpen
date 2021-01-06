5f964f50b9dde6874d5a866bb706b5977be9b994
Salla S <47078225+sallasal@users.noreply.github.com>
2021-01-06 20:20:17 +0200

    Add exercise 1.7

--- Osa-1/unicafe/src/index.js
+++ Osa-1/unicafe/src/index.js
@@ -17,11 +17,38 @@ const StatRow = ({ text, value }) => {
   )
 }
 
+const Avg = ({ good, neutral, bad }) => {
+  let avg = 0
+  
+  if (good + neutral + bad === 0) {
+    avg = 0
+  } else (
+    avg = (good - bad) / (good + neutral + bad)
+  )
+
+  return avg
+}
+
+const Percent = ({ good, neutral, bad }) => {
+  let percent = 0
+
+  if (good + neutral + bad === 0) {
+    percent = 0
+  } else {
+    percent = good*100/(good + neutral + bad)
+  }
+
+  return percent
+}
+
 const App = () => {
   // tallenna napit omaan tilaansa
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
+
+  let average = <Avg good = {good} neutral = {neutral} bad = {bad} /> 
+  let positive = <Percent good = {good} neutral = {neutral} bad = {bad} />
 
   return (
     <div>
@@ -33,6 +60,10 @@ const App = () => {
       <StatRow text = 'Good' value={good} />
       <StatRow text = 'Neutral' value = {neutral} />
       <StatRow text = 'Bad' value = {bad} />
+      <StatRow text = 'All' value = {bad + neutral + good} />
+      <StatRow text = 'Average' value = {average} />
+      <StatRow text = 'Positive' value = {positive} />
+
     </div>
   )
 }
