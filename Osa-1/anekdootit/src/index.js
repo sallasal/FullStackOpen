1b0daf71ddfd1ae1c94113582522ced870c104a9
Salla S <47078225+sallasal@users.noreply.github.com>
2021-01-07 09:54:30 +0200

    Add exercies 1.14

--- Osa-1/anekdootit/src/index.js
+++ Osa-1/anekdootit/src/index.js
@@ -1,22 +1,42 @@
 import React, { useState } from 'react'
 import ReactDOM from 'react-dom'
 
+const Header = ({ text }) => (
+  <h1>{text}</h1>
+)
+
+const MostVoted = ({ text, voteCount }) => {
+  return (
+    <div>
+      <p>{text}</p>
+      <p>has {voteCount} votes</p>
+    </div>
+  )
+}
+
 const App = (props) => {
   const [selected, setSelected] = useState(0)
   const [votes, addVote] = useState(new Array(6+1).join('0').split('').map(parseFloat))
+  const [largest, changeLargest] = useState(0)
 
   const handleVoteClick = () => {
     const copy = [...votes]
     copy[selected] = copy[selected] + 1
+    if (copy[selected] > copy[largest]) {
+      changeLargest(selected)
+    }
     addVote(copy)
   }
 
   return (
     <div>
+      <Header text = 'Anecdote of the day'/>
       <p>{props.anecdotes[selected]}</p>
       <p>Has {votes[selected]} votes</p>
       <button onClick = {() => setSelected(Math.floor(Math.random() * 6))}>Get new</button>
       <button onClick = {handleVoteClick}>Vote</button>
+      <Header text = 'Most voted anecdote'/>
+      <MostVoted text = {props.anecdotes[largest]} voteCount = {votes[largest]} />
     </div>
   )
 }
