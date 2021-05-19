7a11ea8471e991c257391757ee11b4f9aa4e9c0b
sallasal <47078225+sallasal@users.noreply.github.com>
2021-05-19 16:08:23 +0300

    Add exercise 4.2

--- Osa-4/index.js
+++ Osa-4/index.js
@@ -2,41 +2,18 @@ const http = require('http')
 const express = require('express')
 const app = express()
 const cors = require('cors')
+const blogsRouter = require('./controllers/blogs')
 const mongoose = require('mongoose')
 
 const config = require('./utils/config')
 const logger = require('./utils/logger')
+const Blog = require('./models/blog')
 
-const blogSchema = mongoose.Schema({
-  title: String,
-  author: String,
-  url: String,
-  likes: Number
-})
-
-const Blog = mongoose.model('Blog', blogSchema)
 mongoose.connect(config.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false, useCreateIndex: true })
 
 app.use(cors())
 app.use(express.json())
-
-app.get('/api/blogs', (request, response) => {
-  Blog
-    .find({})
-    .then(blogs => {
-      response.json(blogs)
-    })
-})
-
-app.post('/api/blogs', (request, response) => {
-  const blog = new Blog(request.body)
-
-  blog
-    .save()
-    .then(result => {
-      response.status(201).json(result)
-    })
-})
+app.use('/api/blogs', blogsRouter)
 
 app.listen(config.PORT, () => {
   logger.info(`Server running on port ${config.PORT}`)
---
+++ Osa-4/models/blog.js
@@ -0,0 +1,18 @@
+const mongoose = require('mongoose')
+
+const blogSchema = mongoose.Schema({
+  title: String,
+  author: String,
+  url: String,
+  likes: Number
+})
+
+blogSchema.set('toJSON', {
+  transform: (document, retObj) => {
+    retObj.id = retObj._id.toString()
+    delete retObj._id
+    delete retObj.__v
+  }
+})
+
+module.exports = mongoose.model('Blog', blogSchema)
