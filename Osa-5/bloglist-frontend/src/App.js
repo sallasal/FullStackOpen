29cab74f384dd269c7e2dc345dea7e41da4adc44
sallasal <47078225+sallasal@users.noreply.github.com>
2021-06-06 11:48:16 +0300

    Add exercise 5.1

--- Osa-5/bloglist-frontend/src/App.js
+++ Osa-5/bloglist-frontend/src/App.js
@@ -1,9 +1,13 @@
 import React, { useState, useEffect } from 'react'
 import Blog from './components/Blog'
 import blogService from './services/blogs'
+import loginService from './services/login'
 
 const App = () => {
   const [blogs, setBlogs] = useState([])
+  const [username, setUsername] = useState('')
+  const [password, setPassword] = useState('')
+  const [user, setUser] = useState(null)
 
   useEffect(() => {
     blogService.getAll().then(blogs =>
@@ -11,9 +15,50 @@ const App = () => {
     )  
   }, [])
 
+  const handleLogin = async (event) => {
+    console.log('here we are')
+    event.preventDefault()
+    try {
+      const user = await loginService.login({
+        username, password
+      })
+      setUser(user)
+      setUsername('')
+      setPassword('')
+    } catch (exception) {
+      alert('Wrong credentials')
+    }
+  }
+
+  const Header = () => {
+    return <div>
+      <h1>Blog application</h1>
+    </div>
+  }
+
+  if (user === null) {
+    return (
+      <div> 
+        <Header />
+        <h2>Login</h2>
+        <form onSubmit={handleLogin}>
+          <div>
+            Username: <input type='text' value={username} name='Username' onChange={ ({ target }) => setUsername(target.value)}/>
+            </div>
+          <div>
+            Password: <input type='password' value={password} name='Password' onChange={ ({ target }) => {setPassword(target.value)}}/>
+          </div>
+          <button type='submit'>Login</button>
+        </form>
+      </div>
+    )
+  }
+
   return (
     <div>
-      <h2>blogs</h2>
+      <Header />
+      <p>{user.name} is logged in.</p>
+      <h2>Blogs</h2>
       {blogs.map(blog =>
         <Blog key={blog.id} blog={blog} />
       )}
