b67b6bdb2690f13e953713ee4cd0419d3a33acdb
sallasal <47078225+sallasal@users.noreply.github.com>
2021-06-10 22:40:50 +0300

    Add all other blog info to be shown except user

--- Osa-5/bloglist-frontend/src/components/Blog.js
+++ Osa-5/bloglist-frontend/src/components/Blog.js
@@ -1,8 +1,40 @@
-import React from 'react'
-const Blog = ({blog}) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>  
-)
+import React, { useState } from 'react'
+
+const Blog = ({blog}) => {
+  const [blogInfo, showAllInfo] = useState(false)
+
+  const blogStyle = {
+    paddingTop: 10,
+    paddingLeft: 2,
+    border: 'solid',
+    borderWidth: 1,
+    marginBottom: 5,
+    width: 500
+  }
+
+  const allInfo = { display: blogInfo ? '' : 'none' }
+  const limitedInfo = { display: blogInfo ? 'none' : '' }
+
+  const showInfo = () => {
+    showAllInfo(!blogInfo)
+  }
+
+  return (
+    <div style={ blogStyle }>
+      <div style={ limitedInfo }>
+        Title: { blog.title }<br />
+        Author: { blog.author }<br />
+        <button onClick={ showInfo }>Show</button>
+      </div> 
+      <div style={ allInfo }>
+        Title: { blog.title }<br />
+        Author: { blog.author }<br />
+        URL: { blog.url }<br />
+        Likes: { blog.likes }<br />
+        <button onClick={ showInfo }>Hide</button>
+      </div>
+    </div>
+  )
+}
 
 export default Blog
