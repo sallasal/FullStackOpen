94acc6bff6e1bee23c7d98e27e866dd7488404e5
sallasal <47078225+sallasal@users.noreply.github.com>
2021-06-15 21:14:25 +0300

    Add exercise 5.18

--- Osa-5/bloglist-frontend/cypress/integration/blog_app.spec.js
+++ Osa-5/bloglist-frontend/cypress/integration/blog_app.spec.js
@@ -1,6 +1,12 @@
 describe('Blog app', function() {
   beforeEach(function() {
     cy.request('POST', 'http://localhost:3003/api/testing/reset')
+    const user = {
+      name: 'Tiina Testi',
+      username: 'ultiinamate',
+      password: 'salasana123'
+    }
+    cy.request('POST', 'http://localhost:3000/api/users/', user)
     cy.visit('http://localhost:3000')
   })
 
@@ -8,5 +14,23 @@ describe('Blog app', function() {
     cy.contains('Login')
     cy.contains('Username:')
     cy.contains('Password:')
+  })
+
+  describe('Login', function() {
+    it('succeeds with correct credentials', function() {
+      cy.get('#username').type('ultiinamate')
+      cy.get('#password').type('salasana123')
+      cy.get('#loginButton').click()
+
+      cy.contains('Tiina Testi is logged in.')
+    })
+
+    it('fails with wrong credentials', function() {
+      cy.get('#username').type('root')
+      cy.get('#password').type('root')
+      cy.get('#loginButton').click()
+
+      cy.get('.error').should('contain', 'Wrong credentials').and('have.css', 'color', 'rgb(255, 0, 0)')
+    })
   })
 })
--- Osa-5/bloglist-frontend/src/App.js
+++ Osa-5/bloglist-frontend/src/App.js
@@ -117,12 +117,12 @@ const App = () => {
         <h2>Login</h2>
         <form onSubmit={handleLogin}>
           <div>
-            Username: <input type='text' value={username} name='Username' onChange={ ({ target }) => setUsername(target.value)}/>
+            Username: <input type='text' value={username} id='username' name='Username' onChange={ ({ target }) => setUsername(target.value)}/>
             </div>
           <div>
-            Password: <input type='password' value={password} name='Password' onChange={ ({ target }) => {setPassword(target.value)}}/>
+            Password: <input type='password' value={password} id='password' name='Password' onChange={ ({ target }) => {setPassword(target.value)}}/>
           </div>
-          <button type='submit'>Login</button>
+          <button id='loginButton' type='submit'>Login</button>
         </form>
       </div>
     )
