57d2bdfb78620175e4f99203386b2914e5791dd2
sallasal <47078225+sallasal@users.noreply.github.com>
2021-06-27 11:01:07 +0300

    Add exercise 6.9

--- Osa-6/redux-anecdotes/src/App.js
+++ Osa-6/redux-anecdotes/src/App.js
@@ -1,6 +1,4 @@
 import React from 'react'
-import { useSelector, useDispatch } from 'react-redux'
-import { voteAnecdote } from './reducers/anecdoteReducer'
 import Anecdotes from './components/AnecdoteList'
 import NewAnecdote from './components/AnecdoteForm'
 
